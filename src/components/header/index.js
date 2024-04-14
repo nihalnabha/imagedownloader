08c799b41f87b9ec46db73c2ad17eff11acb153e
vinay-works <vinayofficial937@gmail.com>
2024-04-15 00:47:10 +0530

    did final changes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 const Header = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+ const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
+ useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       if (scrollPosition > 0) {
@@ -18,21 +18,21 @@ const Header = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+ }, []);
 
-  return (
+ return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 p-4 flex justify-between items-center transition-all ${
         isScrolled ? 'bg-white bg-opacity-90 text-black shadow-md' : 'bg-transparent text-white'
       }`}
     >
       {/* Logo Section */}
-      <div className="flex items-center">
+      <div className="flex items-center justify-center md:justify-start w-full">
         <Image src="/Logo.png" alt="Logo" width={40} height={40} />
         <span className="font-semibold text-lg"></span>
       </div>
     </header>
-  );
+ );
 };
 
 export default Header;
